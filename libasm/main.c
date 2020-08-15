5a6df4d7240a3743da250549266efc41c5df208a
Jonny Saykosy <contact@josayko.com>
2020-08-15 16:32:13 +0200

    add colors to main.c and update Makefile

--- libasm/main.c
+++ libasm/main.c
@@ -6,17 +6,26 @@
 /*   By: jonny <josaykos@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/12 14:46:13 by jonny             #+#    #+#             */
-/*   Updated: 2020/08/15 14:58:48 by jonny            ###   ########.fr       */
+/*   Updated: 2020/08/15 16:31:28 by jonny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 ssize_t ft_write(int fd, const void *buf, size_t count);
+ssize_t ft_read(int fd, void *buf, size_t count);
 
 int main(void)
 {
-	ft_write(1, "Hello\n", 6);
+	int ret;
+
+	write(1, "\033[1;32mMINE : \033[0m", 18);
+	ret = ft_write(1, "Hello", 6);
+	printf(" | ret = %d\n", ret);
+	write(1, "\033[1;31mREAL : \033[0m", 18);
+	ret = write(1, "Hello", 6);
+	printf(" | ret = %d\n", ret);
 	return (0);
 }
---
+++ libasm/src/ft_read.s
@@ -0,0 +1,9 @@
+; vi:syntax=nasm
+
+section .text
+		global ft_read
+
+ft_read
+		mov rax, 0
+		syscall
+		ret
